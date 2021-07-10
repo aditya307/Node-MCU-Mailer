ae6aa50e2504331083f11c5e29d83d221efc81e7
Aditya Yadav <adi.2002.gwl@gmail.com>
2021-07-10 22:38:38 +0530

    Final Commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyPraser = require('body-parser');
+
+//Tranaspoter
+let transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: 'akhlesh.6985@gmail.com',
+    pass: 'nitinyadav',
+  },
+});
+
+const app = express();
+app.use(bodyPraser.json());
+
+app.get('/', async (req, res) => {
+  res.send('Heyy !!');
+});
+
+app.get('/sendmail', async (req, res) => {
+  let mailInfo = await transporter.sendMail({
+    from: 'adi.2002.gwl@gmail.com',
+    to: 'sharmaajay2kuu@gmail.com',
+    subject: 'NODE MCU check',
+    html: `<p><b>What If...?</b> ka trailer dekha tune?</p>`,
+  });
+  console.log('Message Sent: %s', mailInfo.messageId);
+  res.send('Mail Sent!!');
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server runnning on ${PORT}`);
+});
